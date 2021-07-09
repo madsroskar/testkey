648d244b4d1b98a1b83eb1a533b7d15dd3a7ac6a
madsroskar <madsroskar@users.noreply.github.com>
2021-07-09 15:28:50 +0000

    deploy: 28d34523afaa8cd854bfca5dddaf51a60be4e193

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the prefix in production and not development.
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
-}
