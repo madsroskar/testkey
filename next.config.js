a74b8b15cfac4aeeaa4539d4ca11b6ff6fc93e57
Mads Røskar <madshvero@gmail.com>
2021-07-09 17:35:34 +0200

    Remove asset prefix

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   // Use the prefix in production and not development.
   reactStrictMode: true,
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  assetPrefix: '',
 }
