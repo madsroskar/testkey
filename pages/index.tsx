1555818e09cfa5089d624602c32f637ed2a9f0da
Mads Røskar <madshvero@gmail.com>
2021-07-10 20:08:20 +0200

    Add favicon to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import {
   Stack,
   HStack,
   Text,
+  useTheme,
 } from "@chakra-ui/react";
 import Head from "next/head";
 import React, { useState, useEffect } from "react";
@@ -169,10 +170,32 @@ const DebugWarning = () =>
 
 export default function Home() {
   const { pressedKeys } = useKeyEvents();
+  const theme = useTheme();
+  console.log({ theme });
   return (
     <>
       <Head>
         <title>Testkey</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        {/* <meta name="theme-color" content={} /> */}
       </Head>
       <DebugWarning />
       <Center width="100vw" mt="10vh" height="80vh">
