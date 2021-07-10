e9f7efae8b8aa2fba94d838f171609fdf0f9636b
madsroskar <madsroskar@users.noreply.github.com>
2021-07-10 10:54:48 +0000

    deploy: b4eb9ef1fa7d808667e7a90251e36357fd2a6d6c

---
+++ _next/static/hdXI5wTEQcHVKiwDqvIgu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-391828952f5b0df02de8.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
