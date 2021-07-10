6b5694877d22c3137b54eeece842d215e9f23e27
Mads Røskar <madshvero@gmail.com>
2021-07-10 14:28:11 +0200

    Add page title, and format with prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,90 +1,102 @@
-import { Badge, Box, Center, Link, Heading, Stack, HStack, Text } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Center,
+  Link,
+  Heading,
+  Stack,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
+import Head from "next/head";
 import React, { useState, useEffect } from "react";
 
-const DEBUG = process.env.NODE_ENV !== "production" && (process.env.NEXT_PUBLIC_DEBUG === "true" || false);
+const DEBUG =
+  process.env.NODE_ENV !== "production" &&
+  (process.env.NEXT_PUBLIC_DEBUG === "true" || false);
 
 const dummyKeys = [
   {
-    "code": "ControlRight",
-    "key": "Control",
-    "ctrl": true,
-    "meta": false,
-    "shift": false,
-    "location": 2
+    code: "ControlRight",
+    key: "Control",
+    ctrl: true,
+    meta: false,
+    shift: false,
+    location: 2,
   },
   {
-    "code": "ControlLeft",
-    "key": "Control",
-    "ctrl": true,
-    "meta": false,
-    "shift": false,
-    "location": 1
+    code: "ControlLeft",
+    key: "Control",
+    ctrl: true,
+    meta: false,
+    shift: false,
+    location: 1,
   },
   {
-    "code": "KeyA",
-    "key": "a",
-    "ctrl": true,
-    "meta": false,
-    "shift": false,
-    "location": 0
+    code: "KeyA",
+    key: "a",
+    ctrl: true,
+    meta: false,
+    shift: false,
+    location: 0,
   },
   {
-    "code": "KeyS",
-    "key": "s",
-    "ctrl": true,
-    "meta": false,
-    "shift": false,
-    "location": 0
+    code: "KeyS",
+    key: "s",
+    ctrl: true,
+    meta: false,
+    shift: false,
+    location: 0,
   },
   {
-    "code": "Quote",
-    "key": "Dead",
-    "ctrl": true,
-    "meta": false,
-    "shift": false,
-    "location": 0
+    code: "Quote",
+    key: "Dead",
+    ctrl: true,
+    meta: false,
+    shift: false,
+    location: 0,
   },
   {
-    "code": "Semicolon",
-    "key": ";",
-    "ctrl": true,
-    "meta": false,
-    "shift": false,
-    "location": 0
+    code: "Semicolon",
+    key: ";",
+    ctrl: true,
+    meta: false,
+    shift: false,
+    location: 0,
   },
   {
-    "code": "Comma",
-    "key": ",",
-    "ctrl": true,
-    "meta": false,
-    "shift": false,
-    "location": 0
+    code: "Comma",
+    key: ",",
+    ctrl: true,
+    meta: false,
+    shift: false,
+    location: 0,
   },
   {
-    "code": "KeyV",
-    "key": "v",
-    "ctrl": true,
-    "meta": false,
-    "shift": false,
-    "location": 0
-  }
+    code: "KeyV",
+    key: "v",
+    ctrl: true,
+    meta: false,
+    shift: false,
+    location: 0,
+  },
 ];
 
 enum Location {
   Standard = 0x00,
   Left = 0x01,
   Right = 0x02,
-  Numpad = 0x03
+  Numpad = 0x03,
 }
 
 type KeyType = {
-  code: string,
-  key: string,
-  ctrl: boolean,
-  meta: boolean,
-  shift: boolean,
-  location: Location
-}
+  code: string;
+  key: string;
+  ctrl: boolean;
+  meta: boolean;
+  shift: boolean;
+  location: Location;
+};
 
 const mapKey = (event: KeyboardEvent): KeyType => ({
   code: event.code,
@@ -92,69 +104,82 @@ const mapKey = (event: KeyboardEvent): KeyType => ({
   ctrl: event.ctrlKey,
   meta: event.metaKey,
   shift: event.shiftKey,
-  location: event.location
+  location: event.location,
 });
-type KeyBoxProps = { k: KeyType }
+type KeyBoxProps = { k: KeyType };
 
 const KeyBox = ({ k }: KeyBoxProps) => (
-  <Box
-    borderWidth={2}
-    border='solid black'
-    borderRadius='10px'
-  >
-    <Heading size='xl'>{k.key}</Heading>
+  <Box borderWidth={2} border="solid black" borderRadius="10px">
+    <Heading size="xl">{k.key}</Heading>
     <Text>{k.code}</Text>
     <HStack>
-      {k.ctrl && (<Badge colorScheme='red'>CTRL</Badge>)}
-      {k.shift && (<Badge colorScheme='green'>Shift</Badge>)}
-      {k.meta && (<Badge colorScheme='yellow'>Meta</Badge>)}
+      {k.ctrl && <Badge colorScheme="red">CTRL</Badge>}
+      {k.shift && <Badge colorScheme="green">Shift</Badge>}
+      {k.meta && <Badge colorScheme="yellow">Meta</Badge>}
     </HStack>
   </Box>
-)
+);
 
 const useKeyEvents = () => {
-  const [pressedKeys, setPressedKeys] = useState<Array<KeyType>>(DEBUG ? dummyKeys : []);
+  const [pressedKeys, setPressedKeys] = useState<Array<KeyType>>(
+    DEBUG ? dummyKeys : []
+  );
 
   useEffect(() => {
     const onKeyDown = (event: KeyboardEvent) => {
       if (!event.repeat) {
-        setPressedKeys((pressedKeys: Array<KeyType>) => [...pressedKeys, mapKey(event)])
+        setPressedKeys((pressedKeys: Array<KeyType>) => [
+          ...pressedKeys,
+          mapKey(event),
+        ]);
       }
-    }
+    };
 
     const onKeyUp = (event: KeyboardEvent) => {
-      setPressedKeys((pressedKeys: Array<KeyType>) => pressedKeys.filter(p => p.code !== event.code))
-    }
+      setPressedKeys((pressedKeys: Array<KeyType>) =>
+        pressedKeys.filter((p) => p.code !== event.code)
+      );
+    };
 
     const resetKeys = () => {
       if (!DEBUG) {
-        setPressedKeys([])
+        setPressedKeys([]);
       }
-    }
+    };
 
-    window.addEventListener('keydown', onKeyDown)
-    window.addEventListener('keyup', onKeyUp)
-    window.addEventListener("blur", resetKeys)
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("blur", resetKeys);
 
     return () => {
-      window.removeEventListener('keydown', onKeyDown)
-      window.removeEventListener('keyup', onKeyUp)
-    }
-  }, [pressedKeys])
+      window.removeEventListener("keydown", onKeyDown);
+      window.removeEventListener("keyup", onKeyUp);
+    };
+  }, [pressedKeys]);
 
   return { pressedKeys };
-}
+};
 
-const DebugWarning = () => DEBUG ? (<Text position='fixed' top={0} left={0} color='red.400'>Debug is active</Text>) : null;
+const DebugWarning = () =>
+  DEBUG ? (
+    <Text position="fixed" top={0} left={0} color="red.400">
+      Debug is active
+    </Text>
+  ) : null;
 
 export default function Home() {
   const { pressedKeys } = useKeyEvents();
   return (
     <>
+      <Head>
+        <title>Testkey</title>
+      </Head>
       <DebugWarning />
-      <Center width='100vw' mt='10vh' height='80vh'>
+      <Center width="100vw" mt="10vh" height="80vh">
         <HStack>
-          {pressedKeys.map((k, i) => (<KeyBox key={i} k={k} />))}
+          {pressedKeys.map((k, i) => (
+            <KeyBox key={i} k={k} />
+          ))}
         </HStack>
       </Center>
       <Stack
@@ -166,10 +191,14 @@ export default function Home() {
         color="gray.500"
       >
         <Text>Fork this on </Text>
-        <Link href="https://github.com/madsroskar/testkey" color='yellow' isExternal>
+        <Link
+          href="https://github.com/madsroskar/testkey"
+          color="yellow"
+          isExternal
+        >
           GitHub
         </Link>
       </Stack>
     </>
-  )
+  );
 }
